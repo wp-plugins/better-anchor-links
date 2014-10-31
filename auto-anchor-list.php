b7becd05e7d9223fd6078b95739fdc4cca628fc9
elpresidento <elpresidento@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-31 17:30:11 +0000

    version 1.7.2
    git-svn-id: https://plugins.svn.wordpress.org/better-anchor-links/trunk@1017556 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- auto-anchor-list.php
+++ auto-anchor-list.php
@@ -4,7 +4,7 @@ Plugin Name: Better Anchor Links
 Plugin URI: http://ludek.org/bal/index.html
 Description: Automatically creates and displays anchor links.
 Author: Luděk Melichar
-Version: 1.7.1
+Version: 1.7.2
 Author URI: http://ludek.org
 */
 
@@ -29,7 +29,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 if (!class_exists('mwm_aalLoader')) {
 	class mwm_aalLoader{
 		
-		var $version     = '1.7.1';
+		var $version     = '1.7.2';
 		var $options     = '';
 		var $links = array();
 	
